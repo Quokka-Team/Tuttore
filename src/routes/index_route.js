2e22de1dde3b8d9ea0a218c7d1cf2ce9127a06ac
CamiloBallen24 <andrescamilo468@gmail.com>
2019-10-06 17:40:19 -0500

    Deploy Completed AWS:tada:

--- src/routes/index_route.js
+++ src/routes/index_route.js
@@ -9,29 +9,36 @@ const auth = require('../middlewares/auth');
 
 const api = express.Router();
 
-api.get('/', (req, res) =>{
+api.get('/', (req, res) => {
     res.send("Estas en al ruta '/' con el metodo get ");
 });
 
-api.post('/', (req, res) =>{
+api.post('/', (req, res) => {
     res.send("Estas en al ruta '/' con el metodo post ");
+});
+
+
+
+api.get('/signUp', (req, res) => {
+    res.send("Estas en al ruta '/signUp' con el metodo get");
 });
 
 api.post('/signUp', StudentController.signUp);
 
-api.post('/signIn', StudentController.signIn);
 
 
-api.get('/profile', auth,  (req, res) =>{
-    res.send("Estas en al ruta '/profile' con el metodo get y te ha autentificado");
-});
-
-api.get('/signIn', (req, res) =>{
+api.get('/signIn', (req, res) => {
     res.send("Estas en al ruta '/singIn' con el metodo get");
 });
 
-api.get('/signUp', (req, res) =>{
-    res.send("Estas en al ruta '/signUp' con el metodo get");
+api.post('/signIn', StudentController.signIn);
+
+
+api.get('/profile', auth, (req, res) => {
+    res.send("Estas en al ruta '/profile' con el metodo get y te ha autentificado");
 });
+
+
+
 
 module.exports = api;
