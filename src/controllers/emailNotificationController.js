edc6c1e225d5251afe514319e72cf124dacf3a4b
CamiloBallen24 <andrescamilo468@gmail.com>
2019-10-18 21:27:53 -0500

    :pencil: first use template send verification code

--- src/controllers/emailNotificationController.js
+++ src/controllers/emailNotificationController.js
@@ -1,7 +1,10 @@
-const AWS = require('../services/aws');
+//const AWS = require('../services/aws');
+
+const AWS = require('../scripts-aws/ses/configSes');
+
 const createTemplateProvisional = require('../templates/templateSendEmailProvisional'); 
 
-async function sendCodeVerification(req, res){
+async function sendCodeVerification2(req, res){
     const email = req.body.email;
     const subject = 'Code Verification Email';
     const number = `${getRandomInt(0,9)}${getRandomInt(0,9)}${getRandomInt(0,9)}${getRandomInt(0,9)}${getRandomInt(0,9)}${getRandomInt(0,9)}`;
@@ -23,7 +26,42 @@ async function sendCodeVerification(req, res){
         });
 }
 
+async function sendCodeVerification(req, res){
+    const email = req.body.email;
+    const number = `${getRandomInt(0,9)}${getRandomInt(0,9)}${getRandomInt(0,9)}${getRandomInt(0,9)}${getRandomInt(0,9)}${getRandomInt(0,9)}`;
+    
+    let replacementTags = {
+        verificationCode : number
+    };
 
+
+    // Create sendTemplatedEmail params 
+    let params = {
+        Destination: { 
+        CcAddresses: [
+            email
+        ],
+        ToAddresses: [
+            email
+        ]
+        },
+        Source: 'QuokkaTeam2019@gmail.com', 
+        Template: 'verificationCodeTemplate', 
+        TemplateData: JSON.stringify(replacementTags), 
+        ReplyToAddresses: [
+        'QuokkaTeam2019@gmail.com'
+        ],
+    };
+    let sendPromise = new AWS.SES({apiVersion: '2010-12-01'}).sendTemplatedEmail(params).promise();
+
+    sendPromise.then(
+        function(data) {
+        res.send(replacementTags);
+    }).catch(
+        function(err) {
+        res.send(err);
+    });
+}
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
